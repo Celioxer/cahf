1d897df283177cc43eb853fb99d5faccf03b48cc
celiors <ti@saocamilopg.com.br>
2025-03-05 12:22:13 -0300

    Cahf

---
+++ src/main/java/cahf/celio/CahfApplication.java
@@ -0,0 +1,14 @@
+package cahf.celio;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CahfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CahfApplication.class, args);
+	}
+
+}
