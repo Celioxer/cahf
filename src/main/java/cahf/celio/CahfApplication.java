d061ed8708a2807f4a13c2d6bb9867c34c61e028
celiors <ti@saocamilopg.com.br>
2025-03-05 17:18:52 -0300

    Cahf

--- src/main/java/cahf/celio/CahfApplication.java
+++ src/main/java/cahf/celio/CahfApplication.java
@@ -9,6 +9,14 @@ public class CahfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CahfApplication.class, args);
+			// Mantém o aplicativo rodando
+			while (true) {
+				try {
+					Thread.sleep(1000); // Espera 1 segundo
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
-}
